a646e70c6c606de9efc79f4b5480df4286c5760f
krishnaji <prahaladsharma4u@gmail.com>
2024-09-04 19:04:47 +0530

    News Screen Designed in Composed

---
+++ app/src/main/java/com/example/data/Constant.kt
@@ -0,0 +1,6 @@
+package com.example.data
+
+const val TOP_HEADLINES = "top-headlines"
+const val COUNTRY = "country}"
+const val TOP_HEADLINES_SOURCES = "top-headlines"
+const val DATA_NOT_AVAILABLE = "Data not available"
--- app/src/main/java/com/example/data/api/NetworkApiService.kt
+++ app/src/main/java/com/example/data/api/NetworkApiService.kt
@@ -1,14 +1,20 @@
 package com.example.data.api
 
+import com.example.data.COUNTRY
+import com.example.data.TOP_HEADLINES
+import com.example.data.TOP_HEADLINES_SOURCES
 import com.example.data.model.TopHeadlinesResponseDTO
 import retrofit2.http.GET
-import retrofit2.http.Header
 import retrofit2.http.Headers
 import retrofit2.http.Query
 
 interface NetworkApiService {
     @Headers("X-Api-Key: 9f6482a584804376874b848980b7a044")
 
-    @GET("top-headlines")
-    suspend fun getTopHeadlines(@Query("country") country: String): TopHeadlinesResponseDTO
+    @GET(TOP_HEADLINES)
+    suspend fun getTopHeadlines(@Query(COUNTRY) country: String): TopHeadlinesResponseDTO
+
+    @GET(TOP_HEADLINES_SOURCES)
+    suspend fun getNesFromSources(@Query("sources") sources: String): TopHeadlinesResponseDTO
+
 }
